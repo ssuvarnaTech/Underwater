b5d29a47a7b3e655df96eeb087d0cf5d73f146f3
ssuvarnaTech <ssuvarna@ucsc.edu>
2021-04-19 14:07:43 -0700

    updated comments  and point breakdown

--- src/main.js
+++ src/main.js
@@ -1,3 +1,13 @@
+//Sreevani Suvarna
+//Project's Title: Ariel's Fight
+//4/19/21
+//Time it took to complete project: 4 days
+//Points BREAKDOWN:
+//Redesign the game's artwork, UI, and sound to change its theme/aesthetic (to something other than sci-fi) 
+// (60):  I changed the artwork to the underwater artwork with an underwater theme UI and sounds that relate to underwater life like bubbles and splashes
+//Create a new spaceship type (w/ new artwork) that's smaller, moves faster, and is worth more points (20) (created new trident and made it move faster)
+//Create a new animated sprite for the Spaceship enemies (10) -> (changed to a firework explosion)
+//Create a new title screen (e.g., new artwork, typography, layout) (10) (added background image)
 let config = {
     type: Phaser.CANVAS,
     width: 640,
